074646f30c23a42299e179fdb6e0d55bafcd5e2e
StergiosAnastasiadis <Stelioslonx@outlook.com>
2024-05-03 02:50:01 +0300

    Create Basic Store

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import './App.css';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/register' element={<Register></Register>} />
-        <Route path='/login' element={<Login></Login>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
+
   );
 }
 
